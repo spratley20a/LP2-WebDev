9a55ec4313eeac5406066aa170c6e9ac7d9a5d70
Feng20T <46931359+Feng20T@users.noreply.github.com>
2019-01-28 19:18:59 -0500

    Update donation.js

--- donation.js
+++ donation.js
@@ -3,3 +3,9 @@ function main()
 {
   let donation = getElementById("donation")
 }
+function submit()
+{
+  let donation = getElementById("donation")
+  let congrats = document.getElementById("congrats")
+  congrats.innerHTML = donation
+}
