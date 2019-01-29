825c3d1dfa5248ced7de5ecc3d12bd97e94d8a97
marcusneacsu <45804217+marcusneacsu@users.noreply.github.com>
2019-01-28 21:43:38 -0500

    Update creative.js

--- creative.js
+++ creative.js
@@ -1,1 +1,19 @@
-
+window.addEventListener("load", main);
+function main()
+{
+  let passwordEnter = document.getElementById("passwordEnter");
+  let passwordOutput = document.getElementById("passwordOutput");
+  let usernameEnter = document.getElementById("usernameEnter");
+  let usernameOutput = document.getElementById("usernameOutput");
+  passwordOutput.textContent = passwordEnter.value;
+  usernameOutput.textContent = usernameEnter.value;
+}
+function submit()
+{
+  let passwordEnter = document.getElementById("passwordEnter");
+  let passwordOutput = document.getElementById("passwordOutput");
+  let usernameEnter = document.getElementById("usernameEnter");
+  let usernameOutput = document.getElementById("usernameOutput");
+  passwordOutput.textContent = passwordEnter.innerHTML;
+  usernameOutput.textContent = usernameEnter.innerText;
+}
