fb9a16502ecb66392344e6b397192c1b9a3481c7
spratley20a <46931207+spratley20a@users.noreply.github.com>
2019-01-28 21:00:26 -0500

    Update donation.js

--- donation.js
+++ donation.js
@@ -1,11 +1,11 @@
 window.addEventListener("load", main);
 function main()
 {
-  let donation = getElementById("donation")
+  let donation = getElementById("donation");
 }
 function submit()
 {
-  let donation = getElementById("donation")
-  let congrats = document.getElementById("congrats")
-  congrats.innerHTML = donation
+  let donation = document.getElementById("donation");
+  let congrats = document.getElementById("congrats");
+  congrats.textContent = donation.value;
 }
