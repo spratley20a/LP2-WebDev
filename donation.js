d049cd88026da6fad938a310309985c58c8d4603
Feng20T <46931359+Feng20T@users.noreply.github.com>
2019-01-28 21:46:57 -0500

    Update donation.js

--- donation.js
+++ donation.js
@@ -1,11 +1,15 @@
 window.addEventListener("load", main);
 function main()
 {
-  let donation = getElementById("donation");
+  let donation = getElementById("donation")
 }
 function submit()
 {
-  let donation = document.getElementById("donation");
-  let congrats = document.getElementById("congrats");
-  congrats.textContent = donation.value;
+  let donation = getElementById("donation")
+  if (Number(donation.value) < 10000000000000 and Number(donation.value) > 0) {
+    let congrats = document.getElementById("congrats");
+    document.getElementById("congrats").innerHTML = "Thank you for donating $" + Number(donation.value);
+  }
+  else {
+    document.getElementById("congrats").innerHTML = "That is not a valid amount";
 }
