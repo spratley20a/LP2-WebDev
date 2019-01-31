a21ddc6c4c373813fcb8dfe706424b13d4554fb9
marcusneacsu <45804217+marcusneacsu@users.noreply.github.com>
2019-01-31 11:35:21 -0500

    Update creative.js

--- creative.js
+++ creative.js
@@ -1,21 +1,31 @@
 window.addEventListener("load", main);
+window.addEventListener("load", letter);
 function main()
 {
+  console.log("function main triggered");
   let passwordEnter = document.getElementById("passwordEnter");
   let passwordOutput = document.getElementById("passwordOutput");
   let usernameEnter = document.getElementById("usernameEnter");
   let usernameOutput = document.getElementById("usernameOutput");
-  let span = docucment.getElementByTagName("span");
-  let paragraph = docucment.getElementByTagName("p");
   passwordOutput.textContent = passwordEnter.value;
   usernameOutput.textContent = usernameEnter.value;
 }
-function submit()
+function letter()
 {
-  let passwordEnter = document.getElementById("passwordEnter");
-  let passwordOutput = document.getElementById("passwordOutput");
-  let usernameEnter = document.getElementById("usernameEnter");
-  let usernameOutput = document.getElementById("usernameOutput");
-  passwordOutput.textContent = passwordEnter.innerHTML;
-  usernameOutput.textContent = usernameEnter.innerText;
+  console.log("function letter loaded");
+  let sizeControl = document.getElementById("sizeControl");
+  let sizeValue = document.getElementById("sizeValue");
+  let action = document.getElementById("action");
+  let p = document.getElementsByTagName("p");
+  let stuff = document.getElementsByTagName("stuff");
+  action.addEventListener("click", e =>
+  {
+    console.log("function action has been triggered");
+    sizeValue.innerText = sizeControl.value;
+    for(let k=0; k < p.length; k++)
+    {
+      p[k].style.fontSize = sizeControl.value + "px";
+      console.log(p[k]);
+    }
+  });
 }
